009856939a5d80bc4b2ddf1d73c69d689ecd7f72
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-10 12:16:53 -0400

    adds exercise that logs new array of city names

--- scripts/cities2.js
+++ scripts/cities2.js
@@ -1,5 +1,4 @@
 // Write a function which takes an array of city objects like the above problem as input and returns an array of the cities names.
-// map
 
 let cities = [
     { name: 'Los Angeles', temperature: 60.0},
@@ -9,4 +8,9 @@ let cities = [
   ];
 
 
-  
+function cityName(arr) {
+    return arr.name;
+}
+
+const cityNames = cities.map(cityName);
+console.log(cityNames);
