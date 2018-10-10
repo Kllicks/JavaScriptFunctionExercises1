4d4e3a2fe8ee263d4768cc89c36314a5912d5921
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-09 23:21:35 -0400

    adds 3times js exercise

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+// You will write a function callNTimes that takes two arguments: times as a number, and fun as a function. It will call that function for that many times. 
+
+// You are allowed to use a loop in the implementation of callNTimes.
+
+function callNTimes (times, fun) {
+
+}
