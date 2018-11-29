69538ecdb2accc639984f86bb1d4023acf5a99dc
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-28 22:21:46 -0500

    refactors the whiteboard problem

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,14 @@
 // array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let array1 = [[0, 1] , [2, 3] , [4, 5, [6, 7, [8, 9]]]];
+let array2 = [];
 
-// let array2 = [...new Set(array1)];
-
-// let array3 = [];
-// let array2 = Array.from(new Set(array1));
-
-function flatten(arr) {
-    return arr.reduce(function (flat, toFlatten) {
-        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+function uniqueValues(arr) {
+    return arr.reduce(function (a, b) {
+        return a.concat(Array.isArray(b) ? uniqueValues(b) : b);
     }, []);
 }
 
-console.log(flatten(array1));
+array2 = uniqueValues(array1);
+
+console.log(array2);
