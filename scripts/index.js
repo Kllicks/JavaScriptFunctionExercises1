26bc828aff7abe757abbdd286ea4034d0d14bf94
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-30 22:20:04 -0500

    gives function proper name

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,12 @@
 let array = [[0, 1] , [2, 3] , [4, 5, [6, 7, [8, 9]]], [10, 11, [12, [13, [14, 15]]]]];
 let array2 = [];
 
-function uniqueValues(arr) {
+function arrayValues(arr) {
     return arr.reduce(function (a, b) {
-        return a.concat(Array.isArray(b) ? uniqueValues(b) : b);
+        return a.concat(Array.isArray(b) ? arrayValues(b) : b);
     }, []);
 }
 
-array2 = uniqueValues(array);
+array2 = arrayValues(array);
 
 console.log(array2);
