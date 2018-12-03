3bb9c7dd0b21eeb6e0420a13ed7c0dcb2be0c342
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-02 21:46:24 -0500

    moves whiteboard question to its own file and started a code wars problem on indexe

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,10 @@
-// You're given an array
-// array = [[0, 1] , [2, 3] , [4, 5, [6, 7, [8, 9]]]]
-// array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// this does not only fix to unique values
-// it prints any value from an array
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
 
-let array = [[0, 1] , [2, 3] , [4, 5, [6, 7, [8, 9]]], [10, 11, [12, [13, [14, 15]]]]];
-let array2 = [];
+// Test.assertDeepEquals(towerBuilder(1), ["*"]);
+// Test.assertDeepEquals(towerBuilder(2), [" * ","***"]);
+// Test.assertDeepEquals(towerBuilder(3), ["  *  "," *** ","*****"]);
 
-function arrayValues(arr) {
-    return arr.reduce(function (a, b) {
-        return a.concat(Array.isArray(b) ? arrayValues(b) : b);
-    }, []);
-}
-
-array2 = arrayValues(array);
-
-console.log(array2);
+function towerBuilder(nFloors) {
+    // build here
+  }
